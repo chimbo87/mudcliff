5642053ade71a859c260af3f87d544999f8abf15
Archford Nhanga <archfordnhanga@gmail.com>
2025-09-22 14:51:29 +0200

    design

--- src/pages/contacts/Contacts.jsx
+++ src/pages/contacts/Contacts.jsx
@@ -3,8 +3,7 @@ import ContactHero from "./components/ContactHero";
 import ContactInformation from "./components/ContactInformation";
 import ContactForm from "./components/ContactForm";
 import ServiceAreasMap from "./components/ServiceAreasMap";
-import BusinessHours from "./components/BusinessHours.JSX";
-// import SocialMediaLinks from "./components/SocialMediaLinks";
+import BusinessHours from "./components/BusinessHours";
 
 function Contacts() {
   return (
@@ -14,7 +13,6 @@ function Contacts() {
       <ContactForm />
       <ServiceAreasMap/>
       <BusinessHours/>
-      {/* <SocialMediaLinks/> */}
     </div>
   );
 }
